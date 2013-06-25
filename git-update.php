b4a1c2e097214609c933d7cba6167514bef72825
Kaspars <kaspars@konstruktors.com>
2013-06-25 16:35:15 +0200

    Add update logs feature

--- git-update.php
+++ git-update.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/kasparsd/git-update
 GitHub URI: https://github.com/kasparsd/git-update
 Description: Provides automatic updates for themes and plugins hosted at GitHub.
 Author: Kaspars Dambis
-Version: 1.2.4
+Version: 1.2.5
 */
 
 
@@ -34,9 +34,7 @@ class GitUpdate {
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'update_check_plugins' ) );
 		add_filter( 'pre_set_site_transient_update_themes', array( $this, 'update_check_themes' ) );
 
-		// This could be used to rename the extracted folder into something
-		// better than repouser-plugin-name-ah7fwc
-		// add_filter( 'upgrader_post_install', array( $this, 'maybe_move_github_folder' ), 10, 3 );
+		add_action( 'core_upgrade_preamble', array( $this, 'show_gitupdate_log' ) );
 
 	}
 
@@ -100,7 +98,7 @@ class GitUpdate {
 		$to_check = array();
 
 		// Run only once an hour. This shouldn't be necessary but some other plugins might call us.
-		// if ( ! isset( $updates->last_checked ) || ( time() - $updates->last_checked ) < HOUR_IN_SECONDS )
+		//if ( ! isset( $updates->last_checked ) || ( time() - $updates->last_checked ) < 60 ) // HOUR_IN_SECONDS
 		//	return $updates;
 
 		// Filter out plugins/themes with known headers
@@ -120,8 +118,28 @@ class GitUpdate {
 					) 
 				);
 
-			if ( is_wp_error( $api_response ) || wp_remote_retrieve_response_code( $api_response ) != 200 )
+			// Check if API responded correctly. Log as error, if not.
+			if ( is_wp_error( $api_response ) || wp_remote_retrieve_response_code( $api_response ) != 200 ) {
+
+				$logs = (array) get_site_option( 'git-update-response-error', array() );
+				
+				// Log only 10 latest error messages
+				array_splice( $logs, 20 );
+
+				// Prepend current error message on top
+				array_unshift(
+						$logs,
+						array(
+							'item' =>  $item,
+							'time' => time(), 
+							'response' => $api_response
+						)
+					);
+
+				update_site_option( 'git-update-response-error', $logs );
+				
 				continue;
+			}
 
 			$response_json = json_decode( wp_remote_retrieve_body( $api_response ), true );
 
@@ -133,7 +151,7 @@ class GitUpdate {
 				if ( version_compare( $tag['name'], $item_details['Version'], '>' ) )
 					$updates->response[ $item ] = (object) array(
 							'new_version' => $tag['name'],
-							'slug' => $item, //dirname( $item ),
+							'slug' => dirname( $item ),
 							'package' => $tag['zipball_url'],
 							'url' => $item_details['PluginURI']
 						);
@@ -185,11 +203,42 @@ class GitUpdate {
 	}
 
 
-	function maybe_move_github_folder( $true, $hook_extra, $result ) {
-		// global $wp_filesystem;
-		// print_r( array( $result, $hook_extra ) );
+	function show_gitupdate_log() {
 
-		return $true;
+		$log_rows = array();
+		$log = get_site_option( 'git-update-response-error', array() );
+
+		if ( empty( $log ) || ! is_array( $log ) )
+			return;
+
+		foreach ( $log as $log_item )
+			$log_rows[] = sprintf( 
+					'<tr>
+						<td><strong>%s</strong><br/>%s</td>
+						<td><pre>%s</pre></td>
+					</tr>',
+					$log_item['item'],
+					date( 'r', $log_item['time'] ),
+					print_r( $log_item['response']['body'], true )
+				);
+
+		if ( empty( $log_rows ) )
+			$log_rows[] = sprintf( 
+					'<tr>
+						<td colspan="2">%s</td>
+					</tr>',
+					__( 'No logs found.', 'git-update' )
+				);
+
+		printf( 
+			'<h3>%s</h3>
+			<table class="widefat">
+				%s
+			</table>',
+			__( 'Git Update Error Logs', 'git-update' ),
+			implode( '', $log_rows ) 
+		);
+
 	}
 
 }
